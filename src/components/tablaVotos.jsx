325403f231434a51aca99ab31151dc8a6c8f9e53
zAlejandro <alejandromolina651@gmail.com>
2025-05-19 14:28:23 -0400

    escel

--- src/components/tablaVotos.jsx
+++ src/components/tablaVotos.jsx
@@ -1,15 +1,43 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import {useEffect, useState} from 'react';
-import {collection, onSnapshot} from 'firebase/firestore';
+import {getDocs, collection, onSnapshot, arrayRemove} from 'firebase/firestore';
 import db from '../firebase';
 import '../App.css';
+
+import { saveAs } from 'file-saver';
+import * as XLSX from 'xlsx';
 
 import {BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer, CartesianGrid} from 'recharts';
 
 function TablaVotos(){
     const [votantes, setVotantes] = useState([]);
     const [conteoVotos, setConteoVotos] = useState({});
+
+
+    async function guardarExcel(){
+        try{
+            const querySnashot = await getDocs(collection(db, 'votantes'));
+            const data = [];
+
+            querySnashot.forEach(doc => {
+                data.push(doc.data());
+            });
+
+            const worksheet = XLSX.utils.json_to_sheet(data);
+            const workbook = XLSX.utils.book_new();
+
+            XLSX.utils.book_append_sheet(workbook,worksheet,'votantes');
+
+            const excelBuffer = XLSX.write(workbook, {bookType: 'xlsx', type: 'array'});
+
+            const blob = new Blob([excelBuffer], {type: 'application/octet-stream'});
+            saveAs(blob, 'votos.xlsx');
+        }catch(e){
+            console.error(e);
+        }
+        
+    }
 
     useEffect(() => {
         const unsubcribe = onSnapshot(collection(db,'votantes'), (snapshot) =>{
@@ -66,13 +94,22 @@ function TablaVotos(){
                 </div>
                 <div className="w-100">
                     <div className="tabla-contenedor mx-auto mt-5 p-4">
-                        <h2 className="text-center mb-4">LISTADO DE VOTOS</h2>
+                        <h2 className="text-center mb-4">
+                            LISTADO DE VOTOS
+                            <button className="btn btn-success align-items-end gap-2" onClick={guardarExcel}>
+                                <i className="bi bi-file-earmark-excel-fill"></i>
+                            </button>
+                        </h2>
+                        
                         <table className="table table-striped table-hover">
                             <thead className="table-dark">
                                 <tr>
                                 <th>#</th>
                                 <th>Cedula</th>
                                 <th>Plancha</th>
+                                <th>
+                                    
+                                </th>
                                 </tr>
                             </thead>
                             <tbody>
